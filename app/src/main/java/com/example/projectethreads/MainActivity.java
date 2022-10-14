ec0eb9f71a3ae1919085659b5925c47318aef357
AlbertFrancisco <albertfr2021@outlook.es>
2022-10-14 19:12:57 +0200

    Fet lo basic, boto per demanar, un text view per mostrar el text i el metode implementat al boto
    Dona error a la linia de String dadesURL=

--- app/src/main/java/com/example/projectethreads/MainActivity.java
+++ app/src/main/java/com/example/projectethreads/MainActivity.java
@@ -3,12 +3,92 @@ package com.example.projectethreads;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+    String error = "";
+    String URL;
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        Button botoEnviar=findViewById(R.id.botoEnviar);
+        TextView vistaResultat=findViewById(R.id.textView);
+        botoEnviar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Log.i("INFO", "S'ha apretat el boto d'enviar");
+                ExecutorService executor = Executors.newSingleThreadExecutor();
+                Handler handler = new Handler(Looper.getMainLooper());
+
+                executor.execute(new Runnable() {
+                    public void run() {
+                        handler.post(new Runnable() {
+                            public void run() {
+                                String dadesURL=getDataFromUrl("https://ieti.cat");
+                                vistaResultat.setText(dadesURL);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    private String getDataFromUrl(String demoIdUrl) {
+        String result = null;
+        int resCode;
+        InputStream in;
+        try {
+            URL url = new URL(demoIdUrl);
+            URLConnection urlConn = url.openConnection();
+
+            HttpsURLConnection httpsConn = (HttpsURLConnection) urlConn;
+            httpsConn.setAllowUserInteraction(false);
+            httpsConn.setInstanceFollowRedirects(true);
+            httpsConn.setRequestMethod("GET");
+            httpsConn.connect();
+            resCode = httpsConn.getResponseCode();
+
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                in = httpsConn.getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        in, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                in.close();
+                result = sb.toString();
+            } else {
+                error += resCode;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
