8f29c56b0afcd6f4478beda7938913f83111e65e
AlbertFrancisco <albertfr2021@outlook.es>
2022-10-17 16:18:17 +0200

    List view implementada, falta formatar el resultat de la URL de manera que cada linia de la URL sigui una linia de la listView

--- app/src/main/java/com/example/projectethreads/MainActivity.java
+++ app/src/main/java/com/example/projectethreads/MainActivity.java
@@ -7,7 +7,9 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -17,6 +19,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -24,8 +27,9 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
     String error = "";
-    String URL;
+    String dadesURL;
 
+    ArrayList<String> liniesURL=new ArrayList<>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +39,23 @@ public class MainActivity extends AppCompatActivity {
         Handler handler = new Handler(Looper.getMainLooper());
 
         Button botoEnviar=findViewById(R.id.botoEnviar);
-        TextView vistaResultat=findViewById(R.id.resultatVista);
+        /*TextView vistaResultat=findViewById(R.id.resultatVista);*/
+        ListView vistaResultat=findViewById(R.id.resultatVista);
+
+        /*Per probar que la list View mostra linies*/
+        liniesURL.add("Linia de mostra1");
+        liniesURL.add("Linia de mostra2");
+        liniesURL.add("mostra de Linia 3");
+        liniesURL.add("La quart4 fil4");
+        liniesURL.add("Linia de mostra5");
+        liniesURL.add("Linia de mostra6");
+        liniesURL.add("mostra de Linia 7");
+        liniesURL.add("La vuitena fila");
+        liniesURL.add("Linia de mostra9");
+        liniesURL.add("Linia de mostra10");
+        liniesURL.add("mostra de Linia 11");
+        liniesURL.add("La dotzena fila");
+
         botoEnviar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Log.i("INFO", "S'ha apretat el boto d'enviar");
@@ -46,8 +66,21 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         //handler.post(new Runnable() {
                             //public void run() {
-                                String dadesURL=getDataFromUrl("https://ieti.cat");
-                                vistaResultat.setText(dadesURL);
+                        dadesURL=getDataFromUrl("https://ieti.cat");
+
+                        ArrayAdapter<String> adaptador=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, liniesURL);
+                        vistaResultat.setAdapter(adaptador);
+                        /*String linia="";
+                        for(int i=0;i<dadesURL.length();i++){
+                            if (String.valueOf(dadesURL.charAt(i)).equals("/"))
+                            linia=linia+String.valueOf(dadesURL.charAt(i));
+                            Log.i("INFO", String.valueOf(dadesURL.charAt(i)));
+                        }
+                        for(String f:liniesURL){
+                            Log.i("INFO", f);
+                        }*/
+                                /*vistaResultat.setText(dadesURL);*/
+
                             //}
                         //});
                     }
